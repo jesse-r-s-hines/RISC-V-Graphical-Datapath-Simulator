f7b562facd95c84b1e627c3613ef29938fb8bb85
Jesse Hines <jessehines@southern.edu>
2021-03-20 13:39:50 -0400

    Switch to Mocha test framework, change target to ES2020
    This allows using BigInt so we can do proper bit manipulation. Javascript number can't store 64 bits precisely as it is a float.
    
    The Jest framework seems to have some issue with "do not know how to serialize a BigInt". Switching to Mocha test framework fixes the issue.

--- tests/demo.test.ts
+++ tests/demo.test.ts
@@ -1,5 +1,8 @@
+import { expect } from 'chai';
 import { sum } from '../src/demo';
 
-test('Test Sum', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('Sum', () => {
+  it("1 + 2 = 3", () => {
+    expect(sum(1, 2)).to.equal(3);
+  })
 });
