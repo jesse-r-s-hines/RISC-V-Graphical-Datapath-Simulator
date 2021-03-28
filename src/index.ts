902b72e34613a0dede2f5b996816d93c34b53828
Jesse Hines <jessehines@southern.edu>
2021-03-28 12:13:50 -0400

    Use webpack
    To run the code in the browser, imports are required to be to ".js" files
    including the extension. However, Mocha can't seem to understand ES6 modules
    and adding the .js extension to the imports breaks it.
    
    Using webpack allows me to have code the Mocha understands, and then bundle it
    into a format with imports that browser will understand. Ugh. This is so
    convoluted.

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// import {Simulator} from "./simulator"
+
+console.log("Hello World")
+
+// let REGS = {
+//     5: 5n,   6: 6n,  7: 7n,
+//    28: 28n, 29:29n, 30: 30n, 31: 31n,
+// }
+
+// let sim = new Simulator([
+//     0x007302b3n, // add x5, x6, x7
+// ], REGS)
+
+// console.log(sim.regFile.registers[5])
+// sim.run()
+// console.log(sim.regFile.registers[5])
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+// see https://webpack.js.org/guides/typescript/
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'source-map',
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'risc-v-simulator.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
