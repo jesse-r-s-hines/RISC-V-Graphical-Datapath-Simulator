1163f364c5ccc74701e8c833f719d706fb381e35
Jesse Hines <jessehines@southern.edu>
2021-04-06 19:41:20 -0400

    Add instruction memory input (it doesn't do anything yet)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import $ from "jquery";
-import {Simulator} from "./simulator"
+import {Simulator} from "./simulator";
 import "bootstrap";
+import CodeMirror from "codemirror";
+import "codemirror/addon/display/placeholder"
 
 // let sim = new Simulator([
 //     0x005003b3n, // add x7, zero, x5
@@ -8,3 +10,9 @@ import "bootstrap";
 // sim.run()
 
 $("#datapath").load("./datapath.svg")
+
+let textarea = <HTMLTextAreaElement>($("#instrMem-editor textarea")[0])
+var editor = CodeMirror.fromTextArea(textarea, {
+    lineNumbers: true,
+    lineNumberFormatter: (l) => "0x" + ((l-1)*4).toString(16).padStart(8, "0")
+});
