3ba5e807faf2d5dfa2a208bd1a5ba81eed1dd285
Jesse Hines <jessehines@southern.edu>
2021-04-07 14:06:36 -0400

    Add memory editor (doesn't do anything yet)

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,26 @@ import "codemirror/addon/display/placeholder"
 // ], {})
 // sim.run()
 
-$("#datapath").load("./datapath.svg")
+$(function() {
+    $("#datapath").load("./datapath.svg")
 
-let textarea = <HTMLTextAreaElement>($("#instrMem-editor textarea")[0])
-var editor = CodeMirror.fromTextArea(textarea, {
-    lineNumbers: true,
-    lineNumberFormatter: (l) => "0x" + ((l-1)*4).toString(16).padStart(8, "0")
-});
+    $("#editor-tabs").on("shown.bs.tab", (event) => {
+        let tab = $( $(event.target).data("bs-target") ).find(".CodeMirror")[0] as any
+        if (tab) tab.CodeMirror.refresh() // We have to refresh the CodeMirror after it is shown
+    })
+    
+    function hexLine(num: number, inc: number, start: number = 0): string {
+        num = (start + num - 1) * inc
+        return "0x" + num.toString(16).padStart(8, "0")
+    }
+    
+    let instrMemEditor = CodeMirror.fromTextArea($("#instrMem-editor textarea")[0] as HTMLTextAreaElement, {
+        lineNumbers: true,
+        lineNumberFormatter: (l) => hexLine(l, 4),
+    });
+    
+    let dataMemEditor = CodeMirror.fromTextArea($("#dataMem-editor textarea")[0] as HTMLTextAreaElement, {
+        lineNumbers: true,
+        lineNumberFormatter: (l) => hexLine(l, 4),
+    });
+})
