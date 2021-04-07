cd467ab93d0d6d25ac4e91465db0b22ed32e70b8
Jesse Hines <jessehines@southern.edu>
2021-04-07 12:49:31 -0400

    Include jQuery as a script
    importing jQuery in the typescript somehow is causing jQuery to be loaded
    twice and breaking everything. @types/jQuery seems to assume jQuery has
    been included in the global scope, so I'm just using a regular script
    include for jQuery.
    
    Splitting jQuery out will also probably make webpack shut up about the
    bundle being too large.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import $ from "jquery";
 import {Simulator} from "./simulator";
 import "bootstrap";
 import CodeMirror from "codemirror";
 import "codemirror/addon/display/placeholder"
+// for some reason importing jquery causes problems. jQuery seems to be getting loaded twice. 
+// @types/jquery assumes you have jquery in global scope so I'm just using a script tag to include jquery.
+// import $ from "jquery"; 
 
 // let sim = new Simulator([
 //     0x005003b3n, // add x7, zero, x5
