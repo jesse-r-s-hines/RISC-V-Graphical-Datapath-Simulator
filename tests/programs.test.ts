22c69d0e3e2799b5c7159d85544ac13804469b6a
Jesse Hines <jessehines@southern.edu>
2021-12-31 14:13:51 -0600

    Add selection sort test

--- tests/programs.test.ts
+++ tests/programs.test.ts
@@ -49,3 +49,39 @@ describe("Bubble Sort", () => {
         }
     })
 })
+
+describe("Selection Sort", () => {
+    let file = './tests/assembly/selectionSort.s'
+    let base = 0x1000_8000n // gp
+
+    it("Basic", () => {
+        let array = [3n, 2n, 6n, 0n, -1n, -2n, 0n].map(e => to_twos_complement(e))
+        let size = BigInt(array.length)
+
+        let sim = makeSim(file, {10: base, 11: size}) // a0, a1
+        sim.dataMem.data.storeArray(base, 4, array)
+
+        sim.run()
+
+        array = sim.dataMem.data.loadArray(base, 4, array.length).map(e => from_twos_complement(e))
+
+        expect(array).to.eql([-2n, -1n, 0n, 0n, 2n, 3n, 6n]);
+    })
+
+    it("Random", () => {
+        let array: bigint[] = []
+        for (let i = 0n; i < 10n; i++) array.push( to_twos_complement(BigInt(Math.floor(Math.random() * (2**32-1)))) )
+
+        let sim = makeSim(file, {10: base, 11: 10n}) // a0, a1
+
+        sim.dataMem.data.storeArray(base, 4, array)
+
+        sim.run()
+
+        array = sim.dataMem.data.loadArray(base, 4, 10).map(e => from_twos_complement(e))
+
+        for (let i = 1; i < 10; i++) {
+            expect(array[0] <= array[1], `${array[0]} < ${array[1]}`).to.be.true
+        }
+    })
+})
