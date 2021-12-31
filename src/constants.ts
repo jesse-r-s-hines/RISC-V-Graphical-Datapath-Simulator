4b6f1dcb2e763a21055046a124a7145c71b7368a
Jesse Hines <jessehines@southern.edu>
2021-12-31 11:56:26 -0600

    Simplify registers constant

--- src/constants.ts
+++ src/constants.ts
@@ -10,12 +10,8 @@ export const registerNames = [
 
 /** Maps register names to their number. */
 export const registers: Record<string, number> = {
-    ...registerNames.reduce((obj, reg, num) => {obj[reg] = num; return obj}, {} as Record<string, number>),
-
-    "x0" :  0,  "x1":  1, "x2" :   2, "x3":   3, "x4":  4,   "x5":  5,  "x6":  6,  "x7":  7,
-    "x8" :  8,  "x9":  9, "x10": 10, "x11": 11, "x12": 12, "x13" : 13, "x14": 14, "x15": 15,
-    "x16": 16, "x17": 17, "x18": 18, "x19": 19, "x20": 20, "x21" : 21, "x22": 22, "x23": 23,
-    "x24": 24, "x25": 25, "x26": 26, "x27": 27, "x28": 28, "x29" : 29, "x30": 30, "x31": 31,
+    ...Object.fromEntries(registerNames.map((reg, num) => [reg, num])),
+    ...Object.fromEntries([...Array(32)].map((_, num) => [`x${num}`, num])),
 }
 
 
