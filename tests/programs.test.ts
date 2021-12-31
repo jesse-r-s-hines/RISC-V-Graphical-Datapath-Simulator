42970a2243edea92a905f277037fcc30829f4f92
Jesse Hines <jessehines@southern.edu>
2021-12-31 12:24:35 -0600

    assemble file in test so errors are caught

--- tests/programs.test.ts
+++ tests/programs.test.ts
@@ -7,16 +7,22 @@ import * as fs from "fs";
 /**
  * Test some actual assembly programs
  */
+
+function makeSim(file: string, regs: Record<number, bigint> = {}): Simulator {
+    let code = fs.readFileSync(file, 'utf8')
+    let machineCode = assemble(code)
+    return new Simulator(machineCode, regs)
+}
+
 describe("Bubble Sort", () => {
-    let code = fs.readFileSync('./tests/assembly/bubbleSort.s', 'utf8')
-    let machine_code = assemble(code)
+    let file ='./tests/assembly/bubbleSort.s'
     let base = 0x1000_8000n // gp
 
     it("Basic", () => {
         let array = [3n, 2n, 1n, 0n, -1n, -2n, -3n].map(e => to_twos_complement(e))
         let size = BigInt(array.length)
 
-        let sim = new Simulator(machine_code, {10: base, 11: size}) // a0, a1
+        let sim = makeSim(file, {10: base, 11: size}) // a0, a1
         sim.dataMem.data.storeArray(base, 4, array)
 
         sim.run()
@@ -30,7 +36,7 @@ describe("Bubble Sort", () => {
         let array: bigint[] = []
         for (let i = 0n; i < 10n; i++) array.push( to_twos_complement(BigInt(Math.floor(Math.random() * (2**32-1)))) )
 
-        let sim = new Simulator(machine_code, {10: base, 11: 10n}) // a0, a1
+        let sim = makeSim(file, {10: base, 11: 10n}) // a0, a1
 
         sim.dataMem.data.storeArray(base, 4, array)
 
