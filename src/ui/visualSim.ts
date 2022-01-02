4ae3b91eb111a2f275b037ac6bf7f936638fc772
Jesse Hines <jessehines@southern.edu>
2022-01-02 13:49:53 -0600

    Move methods into new radix util module

--- src/ui/visualSim.ts
+++ src/ui/visualSim.ts
@@ -1,8 +1,9 @@
 import { Simulator } from "simulator/simulator";
-import { Bits, fromTwosComplement, toTwosComplement } from "utils/bits"
+import { Bits } from "utils/bits"
 import { TruthTable } from "utils/truthTable"
 import { registerNames } from "simulator/constants"
 import { assemble, assembleKeepLineInfo } from "assembler/assembler"
+import { Radix, parseInt, intToStr } from "utils/radix"
 
 import CodeMirror from "codemirror";
 import "codemirror/addon/display/placeholder"
@@ -13,66 +14,16 @@ import "tippy.js/dist/tippy.css";
 import toastr from "toastr";
 
 type CodeMirror = CodeMirror.Editor
-type Radix = "hex" | "bin" | "signed" | "unsigned"
-
-// Some utility methods
-
-/**
- * Takes a string an converts into into a positive bigint with the given radix. Throw exception if fails.
- * Strings with "0x" and "0b" prefixes will be interpreted as hex and binary regardless of radix.
- * @param bits the number of bits the output will be. 
- */
-function parseInt(str: string, radix: Radix, bits: number): bigint {
-    try {
-        if (radix == "hex") {
-            var num = BigInt( /^0[xb]/.test(str) ? str : `0x${str}` )
-        } else if (radix == "bin") {
-            var num = BigInt( /^0[xb]/.test(str) ? str : `0b${str}` )
-        } else if (radix == "signed") {
-            var num =  toTwosComplement(BigInt(str), bits)
-        } else { // (radix == "unsigned")
-            var num =  BigInt(str)
-        }
-        if (num < 0n || num >= 2n ** BigInt(bits)) throw Error() // just trigger catch.
-    } catch { // Int to big or parsing failed
-        throw Error(`"${str}" is invalid. Expected a ${bits} bit ${radix} integer.`)
-    }
-    return num
-}
-
-/**
- * Outputs a string from an positive bigint or Bits with the radix.
- * @param bits the number of bits the output will be. If omitted and you pass a Bits, it 
- *             will get the size from the bits, otherwise it will default to 32.
- */
- function intToStr(num: bigint|Bits, radix: string, bits?: number): string {
-    if (num instanceof Array) {
-        bits = bits ?? num.length
-        num = Bits.toInt(num)
-    } else {
-        bits = bits ?? 32
-    }
-
-    if (radix == "hex") {
-        return "0x" + num.toString(16).toUpperCase().padStart(Math.ceil(bits / 4), "0")
-    } else if (radix == "bin") {
-        return "0b" + num.toString(2).padStart(bits, "0")
-    } else if (radix == "signed") {
-        return fromTwosComplement(num, bits).toString()
-    } else { // (radix == "unsigned")
-        return num.toString()
-    }
-}
 
 /** Returns html showing num as hex, signed, and unsigned */
-function intToAll(num: bigint|Bits, bits: number = 32): string {
+export function intToAll(num: bigint|Bits, bits: number = 32): string {
     let radices = [["Hex", "hex"], ["Unsigned", "unsigned"], ["Signed", "signed"]]
     let lines = radices.map(([l, r]) => `${l}: ${intToStr(num, r, bits)}`)
     return lines.join("<br/>")
 }
 
 /** Converts a line number into a hex address. */
-function hexLine(num: number, inc: number, start: bigint = 0n): string {
+export function hexLine(num: number, inc: number, start: bigint = 0n): string {
     let numB = start + BigInt((num - 1) * inc)
     return intToStr(numB, "hex")
 }
---
+++ src/utils/radix.ts
@@ -0,0 +1,53 @@
+/** Converting numbers and strings between different numeric representations */
+
+import { Bits, fromTwosComplement, toTwosComplement } from "utils/bits"
+
+export type Radix = "hex" | "bin" | "signed" | "unsigned"
+
+/**
+ * Takes a string an converts into into a positive bigint with the given radix. Throw exception if fails.
+ * Strings with "0x" and "0b" prefixes will be interpreted as hex and binary regardless of radix.
+ * @param bits the number of bits the output will be. 
+ */
+export function parseInt(str: string, radix: Radix, bits: number): bigint {
+    try {
+        if (radix == "hex") {
+            var num = BigInt( /^0[xb]/.test(str) ? str : `0x${str}` )
+        } else if (radix == "bin") {
+            var num = BigInt( /^0[xb]/.test(str) ? str : `0b${str}` )
+        } else if (radix == "signed") {
+            var num =  toTwosComplement(BigInt(str), bits)
+        } else { // (radix == "unsigned")
+            var num =  BigInt(str)
+        }
+        if (num < 0n || num >= 2n ** BigInt(bits)) throw Error() // just trigger catch.
+    } catch { // Int to big or parsing failed
+        throw Error(`"${str}" is invalid. Expected a ${bits} bit ${radix} integer.`)
+    }
+    return num
+}
+
+/**
+ * Outputs a string from an positive bigint or Bits with the radix.
+ * @param bits the number of bits the output will be. If omitted and you pass a Bits, it 
+ *             will get the size from the bits, otherwise it will default to 32.
+ */
+export function intToStr(num: bigint|Bits, radix: string, bits?: number): string {
+    if (num instanceof Array) {
+        bits = bits ?? num.length
+        num = Bits.toInt(num)
+    } else {
+        bits = bits ?? 32
+    }
+
+    if (radix == "hex") {
+        return "0x" + num.toString(16).toUpperCase().padStart(Math.ceil(bits / 4), "0")
+    } else if (radix == "bin") {
+        return "0b" + num.toString(2).padStart(bits, "0")
+    } else if (radix == "signed") {
+        return fromTwosComplement(num, bits).toString()
+    } else { // (radix == "unsigned")
+        return num.toString()
+    }
+}
+
