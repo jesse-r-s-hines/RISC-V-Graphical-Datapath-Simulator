b75fa1735a4bb2fff905ea8641f490b5452e1660
Jesse Hines <jessehines@southern.edu>
2022-01-10 22:03:00 -0500

    Add comments to svgo.config.js
    Add all the available plugins we can use.

--- svgo.config.js
+++ svgo.config.js
@@ -14,16 +14,25 @@ module.exports = {
         // Note: since we are manipulating datapath.svg via javascript we have to be careful which optimizations we use.
         //       datapath.svg has unused defs and hidden/empty elements which will be used in javscript, so we can't remove
         //       them, and we can't change the structure of the SVG much.
+
         "cleanupAttrs",                   // cleanup attributes from newlines, trailing, and repeating spaces
         "mergeStyles",                    // merge multiple style elements into one
+        // "inlineStyles",                // move and merge styles from <style> elements to element style attributes
         "removeDoctype",                  // remove doctype declaration
         "removeXMLProcInst",              // remove XML processing instructions
         "removeComments",                 // remove comments
         "removeMetadata",                 // remove <metadata>
         "removeTitle",                    // remove <title>
         "removeDesc",                     // remove <desc>
+        // "removeUselessDefs",           // remove elements of <defs> without id
+        // "removeXMLNS",                 // removes the xmlns attribute (for inline SVG)
         "removeEditorsNSData",            // remove editors namespaces, elements, and attributes
         "removeEmptyAttrs",               // remove empty attributes
+        // "removeHiddenElems",           // remove hidden elements
+        // "removeEmptyText",             // remove empty Text elements
+        // "removeEmptyContainers",       // remove empty Container elements
+        // "removeViewBox",               // remove viewBox attribute when possible
+        // "cleanupEnableBackground",     // remove or cleanup enable-background attribute when possible
         "minifyStyles",                   // minify <style> elements content with CSSO
         "convertColors",                  // convert colors (from rgb() to #rrggbb, from #rrggbb to #rgb)
         "convertPathData",                // convert Path data to relative or absolute (whichever is shorter), convert one segment to another, trim useless delimiters, smart rounding, and much more
@@ -32,11 +41,28 @@ module.exports = {
         "removeNonInheritableGroupAttrs", // remove non-inheritable group's "presentation" attributes
         "removeUselessStrokeAndFill",     // remove useless stroke and fill attributes
         "removeUnusedNS",                 // remove unused namespaces declaration
+        // "prefixIds",                   // prefix IDs and classes with the SVG filename or an arbitrary string
+        // "cleanupIDs",                  // remove unused and minify used IDs
         "cleanupNumericValues",           // round numeric values to the fixed precision, remove default px units
+        // "cleanupListOfValues",         // round numeric values in attributes that take a list of numbers (like viewBox or enable-background)
         "moveElemsAttrsToGroup",          // move elements' attributes to their enclosing group
         "moveGroupAttrsToElems",          // move some group attributes to the contained elements
+        // "collapseGroups",              // collapse useless groups
+        // "removeRasterImages",          // remove raster images
+        // "mergePaths",                  // merge multiple Paths into one
+        // "convertShapeToPath",          // convert some basic shapes to <path>
+        // "convertEllipseToCircle",      // convert non-eccentric <ellipse> to <circle>
         "sortAttrs",                      // sort element attributes for epic readability
         "sortDefsChildren",               // sort children of <defs> in order to improve compression
         "removeDimensions",               // remove width/height and add viewBox if it's missing (opposite to removeViewBox, disable it first)
+        // "removeAttrs",                 // remove attributes by pattern
+        // "removeAttributesBySelector",  // removes attributes of elements that match a CSS selector
+        // "removeElementsByAttr",        // remove arbitrary elements by ID or className
+        // "addClassesToSVGElement",      // add classnames to an outer <svg> element
+        // "addAttributesToSVGElement",   // adds attributes to an outer <svg> element
+        // "removeOffCanvasPaths",        // removes elements that are drawn outside of the viewbox
+        // "removeStyleElement",          // remove <style> elements
+        // "removeScriptElement",         // remove <script> elements
+        // "reusePaths",                  // Find duplicated elements and replace them with links
     ]
   }
