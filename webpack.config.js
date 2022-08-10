f49bd3af4c7b887e4940add48423e2dd1554e3ff
Jesse Hines <jesse.r.s.hines@gmail.com>
2022-08-10 15:14:11 -0400

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -82,7 +82,7 @@ module.exports = (env) => ({
     minimize: env.prod ? true : false // Debugger has trouble if you minify, even with the source map.
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    static: path.join(__dirname, 'dist'),
   },
   externals: env.test ? [require('webpack-node-externals')()] : undefined, // in order to ignore all modules in node_modules folder
 });
