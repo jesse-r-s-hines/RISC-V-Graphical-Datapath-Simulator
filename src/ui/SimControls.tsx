0434486df54ea3cac016f2fca09647a51325760b
Jesse Hines <jesse.r.s.hines@gmail.com>
2023-04-23 14:42:44 -0400

    Call callbacks

--- src/ui/SimControls.tsx
+++ src/ui/SimControls.tsx
@@ -23,25 +23,28 @@ export default function SimControls({state, ...props}: Props) {
         <div className="sim-controls card">
             <div className="card-body d-flex flex-row">
                 {(state == "playing") ? (
-                    <Button variant="" size="sm" title="Pause Simulation">
+                    <Button variant="" size="sm" title="Pause Simulation" onClick={props.onPause}>
                         <i className="fas fa-pause text-warning"></i>
                     </Button>
                 ) : (<>
-                    <Button variant="" size="sm" title="Run Simulation" disabled={state == "done"} >
+                    <Button variant="" size="sm" title="Run Simulation" disabled={state == "done"} onClick={props.onPlay}>
                         <i className="fas fa-play text-success"></i>
                     </Button>
-                    <Button variant="" size="sm" title="Step Simulation" disabled={state == "done"}>
+                    <Button variant="" size="sm" title="Step Simulation" disabled={state == "done"} onClick={props.onStep}>
                         <i className="fas fa-step-forward text-success"></i>
                     </Button>
                 </>)}
                 {(state != "unstarted") ? (
-                    <Button variant="" size="sm" title="Restart Simulation">
+                    <Button variant="" size="sm" title="Reset Simulation" onClick={props.onReset}>
                         <i className="fas fa-sync text-danger"></i>
                     </Button>
                 ) : ""}
                 <div className="flex-grow-1"> {/* Spacer, even if the slider is hidden */}
                     {(state == "playing") ? ( // 2**x steps per second
-                        <input type="range" className="form-range" title="Speed" min={-2} max={4} value={speedTick}/>
+                        <input type="range" className="form-range" title="Speed" min={-2} max={4}
+                            value={speedTick}
+                            onChange={e => props.onSpeedChange?.(+e.target.value)}
+                        />
                     ) : ""}
                 </div>
   
