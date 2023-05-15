77d51d68ac4d18e0945f34a75d0ad35c86f8fad3
Jesse Hines <jesse.r.s.hines@gmail.com>
2023-05-14 21:34:48 -0400

    Fix programs.test.ts

--- tests/programs.test.ts
+++ tests/programs.test.ts
@@ -19,34 +19,33 @@ describe("Bubble Sort", () => {
     const base = 0x1000_8000n // gp
 
     it("Basic", () => {
-        let array = [3n, 2n, 1n, 0n, -1n, -2n, -3n].map(e => toTwosComplement(e, 32))
-        const size = BigInt(array.length)
+        const input = [3n, 2n, 1n, 0n, -1n, -2n, -3n].map(e => toTwosComplement(e, 32))
 
-        const sim = makeSim(file, {10: base, 11: size}) // a0, a1
-        sim.dataMem.data.storeArray(base, 4, array)
+        const sim = makeSim(file, {10: base, 11: BigInt(input.length)}) // a0, a1
+        sim.dataMem.data.storeArray(base, 4, input)
 
         sim.run()
 
-        array = sim.dataMem.data.loadArray(base, 4, array.length).map(e => fromTwosComplement(e, 32))
+        const output = sim.dataMem.data.loadArray(base, 4, input.length).map(e => fromTwosComplement(e, 32))
 
-        expect(array).to.eql([-3n, -2n, -1n, 0n, 1n, 2n, 3n]);
+        expect(output).to.eql([-3n, -2n, -1n, 0n, 1n, 2n, 3n]);
     })
 
     it("Random", () => {
-        let array: bigint[] = []
+        const input: bigint[] = []
         for (let i = 0n; i < 10n; i++)
-            array.push( toTwosComplement(BigInt(Math.floor(Math.random() * (2**32-1))), 32) )
+            input.push( toTwosComplement(BigInt(Math.floor(Math.random() * (2**32-1))), 32) )
 
-        const sim = makeSim(file, {10: base, 11: 10n}) // a0, a1
+        const sim = makeSim(file, {10: base, 11: BigInt(input.length)}) // a0, a1
 
-        sim.dataMem.data.storeArray(base, 4, array)
+        sim.dataMem.data.storeArray(base, 4, input)
 
         sim.run()
 
-        array = sim.dataMem.data.loadArray(base, 4, 10).map(e => fromTwosComplement(e, 32))
+        const output = sim.dataMem.data.loadArray(base, 4, input.length).map(e => fromTwosComplement(e, 32))
 
         for (let i = 1; i < 10; i++) {
-            expect(array[0] <= array[1], `${array[0]} < ${array[1]}`).to.be.true
+            expect(output[i - 1] <= output[i], `${output[i - 1]} < ${output[i]}`).to.be.true
         }
     })
 })
@@ -56,34 +55,33 @@ describe("Selection Sort", () => {
     const base = 0x1000_8000n // gp
 
     it("Basic", () => {
-        let array = [3n, 2n, 6n, 0n, -1n, -2n, 0n].map(e => toTwosComplement(e, 32))
-        const size = BigInt(array.length)
+        const input = [3n, 2n, 6n, 0n, -1n, -2n, 0n].map(e => toTwosComplement(e, 32))
 
-        const sim = makeSim(file, {10: base, 11: size}) // a0, a1
-        sim.dataMem.data.storeArray(base, 4, array)
+        const sim = makeSim(file, {10: base, 11: BigInt(input.length)}) // a0, a1
+        sim.dataMem.data.storeArray(base, 4, input)
 
         sim.run()
 
-        array = sim.dataMem.data.loadArray(base, 4, array.length).map(e => fromTwosComplement(e, 32))
+        const output = sim.dataMem.data.loadArray(base, 4, input.length).map(e => fromTwosComplement(e, 32))
 
-        expect(array).to.eql([-2n, -1n, 0n, 0n, 2n, 3n, 6n]);
+        expect(output).to.eql([-2n, -1n, 0n, 0n, 2n, 3n, 6n]);
     })
 
     it("Random", () => {
-        let array: bigint[] = []
+        const input: bigint[] = []
         for (let i = 0n; i < 10n; i++)
-            array.push( toTwosComplement(BigInt(Math.floor(Math.random() * (2**32-1))), 32) )
+            input.push( toTwosComplement(BigInt(Math.floor(Math.random() * (2**32-1))), 32) )
 
-        const sim = makeSim(file, {10: base, 11: 10n}) // a0, a1
+        const sim = makeSim(file, {10: base, 11: BigInt(input.length)}) // a0, a1
 
-        sim.dataMem.data.storeArray(base, 4, array)
+        sim.dataMem.data.storeArray(base, 4, input)
 
         sim.run()
 
-        array = sim.dataMem.data.loadArray(base, 4, 10).map(e => fromTwosComplement(e, 32))
+        const output = sim.dataMem.data.loadArray(base, 4, 10).map(e => fromTwosComplement(e, 32))
 
-        for (let i = 1; i < 10; i++) {
-            expect(array[0] <= array[1], `${array[0]} < ${array[1]}`).to.be.true
+        for (let i = 1; i < input.length; i++) {
+            expect(output[i - 1] <= output[i], `${output[i - 1]} < ${output[i]}`).to.be.true
         }
     })
 })
